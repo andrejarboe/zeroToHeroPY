946d9ba774c53c0878df0a4219350b1f6b2d9932
Andre Jarboe II <andrejarboe@gmail.com>
2024-09-11 20:15:58 -0500

    is upper is lower

--- hello.py
+++ hello.py
@@ -1,19 +1,6 @@
-def count_primes(num):
-    count = 0 
-    index = 0
+def ran_bool(num,low,high):
+    return low <= num and high >= num
 
-    primes = [2]
-    x = 3
-    if num < 2:  # for the case of num = 0 or 1
-        return 0
-    while x <= num:
-        for y in range(3,x,2):
-            if x % y == 0:
-                break
-            else:
-                primes.append(x)
-                x += 2
-    print(primes)
-    return len(primes)
-
-count_primes(100)
+ran_bool(3,1,10)
+ran_bool(33,1,10)
+ran_bool(3,1,10)
