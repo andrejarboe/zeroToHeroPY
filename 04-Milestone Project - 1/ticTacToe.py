02f884963541629fcce367640384e9091d3af45b
andrejarboe <andrejarboe@gmail.com>
2024-09-17 20:20:20 -0500

    print board

--- 04-Milestone Project - 1/ticTacToe.py
+++ 04-Milestone Project - 1/ticTacToe.py
@@ -1,14 +1,19 @@
 game_on = True
 
 def display_board(board):
-    # board = ['#','#','#','#','#','#','#','#','#','#']
+    # board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
     print(f"        |        |       ")
     print(f"   {board[0]}    |   {board[1]}    |   {board[2]}    ")
     print(f"________|________|_______")
+
     print(f"        |        |       ")
     print(f"   {board[3]}    |   {board[4]}    |   {board[5]}    ")
+    print(f"________|________|_______")
+
+    print(f"        |        |       ")
+    print(f"   {board[6]}    |   {board[7]}    |   {board[8]}    ")
     print(f"        |        |       ")
 
 # while game_on:
-test_board = [' ',' ',' ','X','O','X','O','X','O','X']
+test_board = [' ',' ','O','X','O','X','O','X','O','X']
 display_board(test_board)
